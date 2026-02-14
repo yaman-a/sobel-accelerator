e64e9e99739bc2d3c06c06cd4add25592b930d69
yaman-a <yaman.ashqar05@gmail.com>
2026-02-14 20:56:30 +1030

    I forgot what i changed

--- sim/main.cpp
+++ sim/main.cpp
@@ -2,11 +2,14 @@
 #include "verilated.h"
 #include <iostream>
 
-int main(int argc, char** argv) {
-    Verilated::commandArgs(argc, argv);
+int main() {
+    // Verilated::commandArgs(argc, argv); 
+    // dont actually need commandline args as its boilerplate
 
-    Vsobel* top = new Vsobel;
+    // Create instance of the sobel module
+    Vsobel* top = new Vsobel; 
 
+    // Set initial states
     top->clk = 0;
     top->rst = 1;
     top->valid_in = 0;
@@ -18,13 +21,15 @@ int main(int argc, char** argv) {
         top->eval();
     }
 
-
+    // Release reset state
     top->rst = 0;
 
+    // Pass through input for 5 cycles
     for (int i = 0; i < 5; i++) {
         top->valid_in = 1;
         top->pixel_in = i * 20;
 
+        // One clock cycle
         top->clk = 0;
         top->eval();
         top->clk = 1;
