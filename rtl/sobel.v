41acabd109fc0ff86289870267ac8c7ac169cfd7
yaman-a <yaman.ashqar05@gmail.com>
2026-02-14 18:26:01 +1030

    passthrough sobel module, not implemented yet

--- rtl/sobel.v
+++ rtl/sobel.v
@@ -6,3 +6,15 @@ module sobel (
     output reg valid_out,
     output reg [7:0] pixel_out
 );
+always @(posedge clk) begin
+    if (rst) begin
+        valid_out <= 0;
+        pixel_out <= 0;
+    end else begin
+        valid_out <= valid_in;
+        pixel_out <= pixel_inl // passthrough for the time being
+    end
+end
+
+
+endmodule
